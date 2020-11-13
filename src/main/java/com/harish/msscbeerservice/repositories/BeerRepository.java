732f7238d98135e13f4118b80843ca0c276c683e
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-13 12:29:05 +0530

    Add Beer Repository. Closes #6

---
+++ src/main/java/com/harish/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.harish.msscbeerservice.repositories;
+
+import com.harish.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
