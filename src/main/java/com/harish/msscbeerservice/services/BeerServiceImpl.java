5163a2fe6ebb715bf35efa07a08447fb440f0ad1
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-16 19:25:00 +0530

    Add ehcache

--- src/main/java/com/harish/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/com/harish/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import com.harish.msscbeerservice.web.model.BeerDto;
 import com.harish.msscbeerservice.web.model.BeerPagedList;
 import com.harish.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerMapper beerMapper;
 
     @Override
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
@@ -67,6 +69,7 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
