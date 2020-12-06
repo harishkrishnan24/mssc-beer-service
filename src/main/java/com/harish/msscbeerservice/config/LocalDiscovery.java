96bc6927989413db60712bd3c5c2215304111f0f
Harish krishnan <harishkrishnan1993@gmail.com>
2020-12-06 18:25:58 +0530

    Eureka config

---
+++ src/main/java/com/harish/msscbeerservice/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package com.harish.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile("local-discovery")
+public class LocalDiscovery {
+}
