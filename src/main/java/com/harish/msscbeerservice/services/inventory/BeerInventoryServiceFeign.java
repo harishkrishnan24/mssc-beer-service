c7538fdf392050f9e02e61f41eb82eaf521dd61e
Harish krishnan <harishkrishnan1993@gmail.com>
2020-12-06 19:41:31 +0530

    Use Feign client

---
+++ src/main/java/com/harish/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
@@ -0,0 +1,36 @@
+package com.harish.msscbeerservice.services.inventory;
+
+import com.harish.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@Profile("local-discovery")
+@RequiredArgsConstructor
+public class BeerInventoryServiceFeign implements BeerInventoryService {
+
+    private final InventoryServiceFeignClient inventoryServiceFeignClient;
+
+    @Override
+    public Integer getOnhandInventory(UUID beerId) {
+        log.info("Calling inventory service - BeerId: " + beerId);
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = inventoryServiceFeignClient.getOnHandInventory(beerId);
+
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+        log.info("BeerId: " + beerId + " On hand is: " + onHand);
+
+        return onHand;
+    }
+}
